ab55ec9a6fd97cd30a4907f04d38bae865808081
Ivan5646 <Mike000027@yandex.ru>
2016-11-03 01:08:21 +0700

    first, started navigation

---
+++ main.js
@@ -0,0 +1,138 @@
+var allQuestions = [
+  {sequence: 1, question: "1. Who is Prime Minister of the United Kingdom?", choices: ["Theresa May", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+  correctAnswer:"Theresa May"},
+  {sequence: 2, question: "2. What is the capital of the Great Britain?", choices: ["Paris", "Warsaw", "London", "Liverpool"], correctAnswer:"London"},
+  {sequence: 3, question: "3. What is the capital of the Russian Federation?", choices: ["Prague", "Minsk", "Washington", "Moscow"], correctAnswer:"Moscow"},
+  {sequence: 4, question: "4. Who was the first man in space", choices: ["Armstrong", "Titov", "Gagarin", "Gorbachev"], correctAnswer:"Gagarin"},
+  {sequence: 5, question: "5. Who is the President of the USA", choices: ["Putin", "Psaki", "Clinton", "Obama"], correctAnswer:"Obama"},
+];
+
+for(var i=0; i<allQuestions.length; i++){  // Display navigation
+  var seqSpan = document.createElement("span");
+  seqSpan.setAttribute("id", "seqSpan");
+  seqSpan.innerHTML = allQuestions[i].sequence;
+  document.getElementById("navigation").appendChild(seqSpan);
+}
+
+getQuestion(); // call the function here to display the first question
+
+var qInd; // question index
+function getQuestion(){ // display question and choices. And all the functionality
+
+  if (qInd===null || qInd===undefined){ 
+    qInd = 0;
+  }
+  if(qInd<allQuestions.length){  // allQuestions.length == 5      Get and display question.
+    var myP = document.createElement("p");
+    myP.setAttribute("id", "myPId");
+    document.getElementById("questionBlock").appendChild(myP); // appends empty paragraph
+    document.getElementById("myPId").innerHTML = allQuestions[qInd].question; // inserts text from the array
+
+    for(var choiceInd=0; choiceInd<allQuestions[qInd].choices.length; choiceInd++){ // Get and display choices.
+      if(qInd>0){  
+        myBox = document.getElementById("myCheckbox") // get the user's answer
+        if(myBox.checked == true){
+          console.log("you checked the box");
+          userAnswerVar = myBox.nextSibling.innerHTML; 
+          allQuestions[qInd-1].userAnswer = userAnswerVar; // create a new property with user's answer
+        }
+
+        var checkboxEl = document.getElementById("myCheckbox"); //removes the checkboxes one by one
+        checkboxEl.parentNode.removeChild(checkboxEl);
+        var choiceEl = document.getElementById("choiceId"); // remove choices
+        choiceEl.parentNode.removeChild(choiceEl);
+      }
+
+      var answer = allQuestions[qInd].userAnswer // Display checkboxes
+      if(answer==allQuestions[qInd].choices[choiceInd]){ // display user's checked checkbox
+        var myCheckbox = document.createElement("input"); 
+      myCheckbox.setAttribute("type", "checkbox");
+        myCheckbox.checked = true // set the checked property. Or  myCheckbox.checked = "checked"
+        myCheckbox.setAttribute("id", "myCheckbox");
+        document.getElementById("choiceBlock").appendChild(myCheckbox);
+      }else{ 
+        var myCheckbox = document.createElement("input"); // display unchecked checkboxes
+        myCheckbox.setAttribute("type", "checkbox");
+        myCheckbox.setAttribute("id", "myCheckbox");
+        document.getElementById("choiceBlock").appendChild(myCheckbox);
+      }
+
+      var currentChoice = allQuestions[qInd].choices[choiceInd]; // Display choices. Get the choice item. 
+      var choiceSpan = document.createElement("span");             // create span element
+      choiceSpan.setAttribute("id", "choiceId");
+      var choiceText = document.createTextNode(currentChoice);   // create text node with choice item as text
+      choiceSpan.appendChild(choiceText);                      // append text to the span 
+      document.getElementById("choiceBlock").appendChild(choiceSpan);
+    } // the end of for loop
+  }else{
+    var allCheckBoxes = document.getElementsByTagName("input");
+    for(var i=0; i<allCheckBoxes.length; i++){
+      if(allCheckBoxes[i].checked == true){    
+        console.log("you checked the box");
+        userAnswerElse = allCheckBoxes[i].nextSibling.innerHTML; 
+        allQuestions[qInd-1].userAnswer = userAnswerElse;
+      }
+    }
+    document.getElementById("choiceBlock").innerHTML = ""
+
+    var nextBtn = document.getElementById("next") // remove next and back buttons    
+    nextBtn.parentNode.removeChild(nextBtn);
+    var backBtn = document.getElementById("back")   
+    backBtn.parentNode.removeChild(backBtn);
+
+     numOfCorrectAnswers = 0;
+    for(var a=0; a<allQuestions.length; a++){
+      if(allQuestions[a].userAnswer===allQuestions[a].correctAnswer){  // compare answers
+        var numOfQuestion = allQuestions[a];
+        numOfCorrectAnswers++;
+      }
+    }
+     result = numOfCorrectAnswers / 5 * 100  // get result in %
+    document.getElementById("questionBlock").innerHTML = "Your result is " + result + "%";  // display result
+  }
+  qInd++; 
+} // the end of the getQuestion()
+
+function back(){
+  document.getElementById("myPId").innerHTML = allQuestions[qInd-2].question;  // remove question. insert the required question
+  document.getElementById("choiceBlock").innerHTML = ""; // remove choices
+  for(var choiceInd=0; choiceInd<allQuestions[qInd-2].choices.length; choiceInd++){ // get the previous choices
+
+    var answer = allQuestions[qInd-2].userAnswer // Display checkboxes
+    if(answer==allQuestions[qInd-2].choices[choiceInd]){ // display user's checked checkbox
+      var myCheckbox = document.createElement("input"); 
+      myCheckbox.setAttribute("type", "checkbox");
+      myCheckbox.checked = true // set the checked property. Or  myCheckbox.checked = "checked"
+      myCheckbox.setAttribute("id", "myCheckbox");
+      document.getElementById("choiceBlock").appendChild(myCheckbox);
+    }else{ 
+      var myCheckbox = document.createElement("input"); // display unchecked checkboxes
+      myCheckbox.setAttribute("type", "checkbox");
+      myCheckbox.setAttribute("id", "myCheckbox");
+      document.getElementById("choiceBlock").appendChild(myCheckbox);
+    }
+
+    var currentChoice = allQuestions[qInd-2].choices[choiceInd]; // Display choices. Get the choice item. 
+    var choiceSpan = document.createElement("span");             // create span element
+    choiceSpan.setAttribute("id", "choiceId");
+    var choiceText = document.createTextNode(currentChoice);   // create text node with choice item as text
+    choiceSpan.appendChild(choiceText);                      // append text to the span 
+    document.getElementById("choiceBlock").appendChild(choiceSpan);
+  }
+  qInd--;
+} // the end of back()
+
+function navigate(){ // need to get the value of the clicked span
+
+}
+
+document.getElementById("next").addEventListener("click", function(){
+  getQuestion();
+});
+document.getElementById("back").addEventListener("click", function(){
+  back();
+});
+/*document.getElementById("seqSpan").addEventListener("click", function(){
+  navigate();
+});*/
+
---
+++ notes.js
@@ -0,0 +1,112 @@
+var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", 
+"Winston Churchill", "Tony Blair"], correctAnswer:0}];
+
+// to access a property of an object...
+allQuestions[0].choices[0]
+
+
+
+
+
+Problems:
+- cannot display a question through a function and click SOLVED
+ function getQuestion(/*question/*, choice*/){ // display question and choices
+  console.log("getQuestion message");
+  var questionPar = document.createElement("p");
+  var text = document.createTextNode(allQuestions[0].question);
+  var textText = document.createTextNode("test");
+  var displayText =questionPar.appendChild(text);
+  questionPar.appendChild(textText);
+  /*questionPar.setAttribute("id", "question");
+  document.getElementById("question").innerHTML = allQuestions[0].question;*/
+  
+  /*for(i=0; i<3; i++){
+    document.getElementById("QuestionChoice").innerHTML = allQuestions[question].choices[i];
+  }*/
+}
+
+document.getElementsByTagName("button")[1].addEventListener("click", function(){
+  getQuestion();
+});
+
+// solving
+var myP = document.createElement("p");
+myP.setAttribute("id", "myParagraph");
+//document.getElementById("myParagraph").innerHTML = "chlen sobachiiy";
+var myId = document.getElementById("myParagraph"); // gives null
+
+var Block = document.getElementById("questionBlock"); // returns the id
+// So it is obvious the script element is loaded after document.getElementById("myParagraph"); or somththing
+
+// worked. I never appneded the element to the document, fucking morron
+var myP = document.createElement("p");
+myP.setAttribute("id", "myParagraph");
+var questionBlock = document.getElementById("questionBlock");
+questionBlock.appendChild(myP);
+myP.innerHTML = "hey hey";
+
+
+- Why is question replaces each other and choices do not? Because the question uses .innerHTML
+- What is the allQuestions array? And what are these items: question, choices and correctAnswer?
+allQuestions is an array that consisits of "nameless" object literals 
+var allQuestions = [
+  {question: "1. Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+  correctAnswer:0},
+  {question: "2. What is the capital of the Great Britain?", choices: ["Paris", "Warsaw", "London", "Liverpool"], correctAnswer:2},
+  {question: "3. What is the capital of the Russian Federation?", choices: ["Prague", "Minsk", "Washington", "Moscow"], correctAnswer:3},
+  {question: "4. Who was first man in space", choices: ["Armstrong", "Titov", "Gagarin", "Gorbachev"], correctAnswer:2},
+  {question: "5. Who is the President of the USA", choices: ["Putin", "Psaki", "Clinton", "Obama"], correctAnswer:3},
+];
+- How to create new item in allQuestions array and add user's answer there? Get reqired object literal allQuestions[0], then using dot notation ...
+allQuestions[0].userAnswer = "this is the answer";
+ - Why qInd-1 in allQuestions[qInd-1].userAnswer
+
+
+
+
+Things to do
+- if you check and do not click next, checked box won't be saved. 
+- allow any number of questions and answers
+
+
+before release
+- add var to make local variables
+- possibly give question function as a callback to the cick event?
+
+Things that are done
+- when you go back then next checkboxex are not checked
+- copmare user's answer with the correct answer 
+- function to get the answer Done
+- last question problem. Not getting answer. 
+At the last interation I got qInd == 5, why?. 
+Not getting answer because now it chehkc only the frist box with Putin choice coz myBoxElse = document.getElementById("myCheckbox")
+(need to get all the inputs one by one. Here it only gets the first one thruogh all iterations)
+forEach does not work
+SOLVED by: var allCheckBoxes = document.getElementsByTagName("input"); if(allCheckBoxes[i].checked == true) ...
+
+
+
+Things to improve
+- diplay question navigation bar
+
+
+
+Improve Your Quiz Application From Earlier:
+Add client-side data validation: make sure the user answers each question before proceeding to the next question.
+Store the quiz questions in an external JSON file.
+Add a “Back” button to allow the user to go back and change her answer. The user can go back up to the first question. For the questions that the user has answered already, be sure to show the radio button selected, so that the user is not forced to answer the questions again, which she has completed.
+Use jQuery to add animation (fade out the current question and fade in the next question).
+Test the quiz on IE 9, and fix any bugs. This will give you a good workout 😉
+
+Add user authentication: allow users to log in, and save their login credentials to local storage (HTML5 browser storage).
+Use cookies to remember the user, and show a “Welcome, ‘First Name'” message when the user returns to the quiz.
+
+
+
+local vars from back()
+    var currentChoice = allQuestions[qInd-2].choices[choiceInd]; // Display choices. Get the choice item. 
+    var choiceSpan = document.createElement("span");             // create span element
+    choiceSpan.setAttribute("id", "choiceId");
+    var choiceText = document.createTextNode(currentChoice);   // create text node with choice item as text
+    choiceSpan.appendChild(choiceText);                      // append text to the span 
+    document.getElementById("choiceBlock").appendChild(choiceSpan);
